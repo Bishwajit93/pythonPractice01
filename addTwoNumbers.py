5edaa896f11b12de7b73a5b644b7c721d8c8e02c
bishwajit93 <bish.karm123@gmail.com>
2024-08-19 22:11:25 +0200

    firstPractice

---
+++ addTwoNumbers.py
@@ -0,0 +1,22 @@
+# num1 = 2
+# num2 = 3
+
+# result = num1+ num2
+
+# print("The sum of", num1, "and", num2, "is", result)
+
+
+# a = 'alice'
+# b = 5
+
+# c = a * b
+# print(c)
+
+print('Hello world')
+print('What is yoour name?')
+name = input()
+print('It is nice to meet you', name)
+print('The length of your name is:', len(name))
+print('What is your age?')
+age = input()
+print('you wull be ', int(age)+1, 'in 1 year')
